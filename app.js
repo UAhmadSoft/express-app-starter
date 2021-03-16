40ab56173fe4924d3affabeeb65bbd1577b6c7bf
U Ahmad <UmadAhmad1928@gmail.com>
2021-03-16 19:00:15 +0500

    Feature : Added DB Configuration

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const express = require('express');
+const morgan = require('morgan');
+
+const DBConnect = require('./utils/dbConnect');
 
 const userRouter = require('./routers/userRouter');
 
 const app = express();
 
+// * Connect DB
+DBConnect();
+
+// * Handle Static Files
+app.use(express.static(`${__dirname}/public`));
+
+// * MiddleWares
+
+//$ Development Logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// * Routes
 app.use('/api/v1/users', userRouter);
 
 const port = process.env.PORT || 3000;
