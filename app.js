7741ac1f6aeb6da7f6b91737f7aa3b41b8e197b1
U Ahmad <UmadAhmad1928@gmail.com>
2021-03-16 19:21:59 +0500

    Feature : Added cors and body parser middlewares

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ dotenv.config({ path: './config.env' });
 
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const DBConnect = require('./utils/dbConnect');
 
 const userRouter = require('./routers/userRouter');
+const globalErrorHandler = require('./middlewares/globalErrorHandler');
 
 const app = express();
 
@@ -18,11 +21,24 @@ app.use(express.static(`${__dirname}/public`));
 
 // * MiddleWares
 
+// $ CORS
+app.use(cors());
+
+// $ Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 //$ Development Logging
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 // * Routes
 app.use('/api/v1/users', userRouter);
+
+//$ Global Error Handler
+app.use(globalErrorHandler);
 
 const port = process.env.PORT || 3000;
 
