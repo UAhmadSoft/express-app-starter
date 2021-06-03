486588ce4dd25fd04e49c3afa4d781e7bbada201
U Ahmad <UmadAhmad1928@gmail.com>
2021-06-03 12:57:57 +0500

    refactored some code

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,110 +3,108 @@ const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
 const filterObj = (obj, ...allowedFields) => {
-   const newObj = {};
-   Object.keys(obj).forEach((el) => {
-      if (allowedFields.includes(el)) newObj[el] = obj[el];
-   });
-   return newObj;
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
 };
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
 
-   const users = await User.find();
-
-   // SEND RESPONSE
-   res.status(200).json({
-      status: 'success',
-      results: users.length,
-      data: {
-         users,
-      },
-   });
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
 });
 
 exports.createUser = catchAsync(async (req, res, next) => {
-   const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      passwordConfirm: req.body.passwordConfirm,
-   });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-   if (!newUser)
-      return res.status(400).json({
-         status: 'failed',
-         message: `Can't create user due to invalid details`,
-      });
+  if (!newUser)
+    return next(
+      new AppError(`
+      Can't create user due to invalid details, 400
+      `)
+    );
 
-   res.status(200).json({
-      status: 'success',
-      user: newUser,
-   });
+  res.status(200).json({
+    status: 'success',
+    user: newUser,
+  });
 });
 
 exports.updateUser = catchAsync(async (req, res, next) => {
-   // 1) Create error if user POSTs password data
-   if (req.body.password || req.body.passwordConfirm) {
-      return next(
-         new AppError(
-            'This route is not for password updates. Please use /updateMyPassword.',
-            400
-         )
-      );
-   }
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
 
-   // 2) Filtered out unwanted fields names that are not allowed to be updated
-   const filteredBody = filterObj(req.body, 'name', 'email');
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
 
-   // 3) Update user document
-   const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      filteredBody,
-      {
-         new: true,
-         runValidators: true,
-      }
-   );
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(
+    req.params.id,
+    filteredBody,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-   res.status(200).json({
-      status: 'success',
-      data: {
-         user: updatedUser,
-      },
-   });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
 });
 
 exports.getMe = catchAsync(async (req, res, next) => {
-   req.params.id = req.user._id;
-   next();
+  req.params.id = req.user._id;
+  next();
 });
 
 exports.getUser = catchAsync(async (req, res, next) => {
-   const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id);
 
-   if (!user)
-      return res.status(404).json({
-         status: 'failed',
-         message: `No User found against id ${req.params.id}`,
-      });
+  if (!user)
+    return next(
+      new AppError(`No User found against id ${req.params.id}`, 404)
+    );
 
-   res.status(200).json({
-      status: 'success',
-      user,
-   });
+  res.status(200).json({
+    status: 'success',
+    user,
+  });
 });
 
 exports.deleteUser = catchAsync(async (req, res, next) => {
-   const deletedUser = await User.findByIdAndDelete(req.params.id);
+  const deletedUser = await User.findByIdAndDelete(req.params.id);
 
-   if (!deletedUser)
-      return res.status(404).json({
-         status: 'failed',
-         message: `No User found against id ${req.params.id}`,
-      });
+  if (!deletedUser)
+    return next(
+      new AppError(`No User found against id ${req.params.id}`, 404)
+    );
 
-   res.status(200).json({
-      status: 'success',
-      user: deletedUser,
-   });
+  res.status(200).json({
+    status: 'success',
+    user: deletedUser,
+  });
 });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,21 +9,21 @@ const router = express.Router();
 router.use(protect); //  protect all router which are comming after this middleware
 
 router
-   .route('/me')
-   .get(userController.getMe, userController.getUser)
-   .patch(userController.getMe, authController.updatePassword);
+  .route('/me')
+  .get(userController.getMe, userController.getUser)
+  .patch(userController.getMe, userController.updateUser);
 
 router.patch('/updatePassword', authController.updatePassword);
 
 router
-   .route('/')
-   .get(userController.getAllUsers)
-   .post(restrictTo('admin'), userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(restrictTo('admin'), userController.createUser);
 
 router
-   .route('/:id')
-   .get(userController.getUser)
-   .patch(userController.updateUser)
-   .delete(restrictTo('admin'), userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(restrictTo('admin'), userController.deleteUser);
 
 module.exports = router;
